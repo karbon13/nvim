8223c012c686265fea9f529786d7ce50468f0fc2
karbon13 <alfa.fingerstyle@gmail.com>
2025-11-17 14:03:29 +0700

    Install Lazy.nvim & some plugins

---
+++ lua/config/settings.lua
@@ -0,0 +1,17 @@
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+vim.opt.wrap = false
+vim.opt.cmdheight = 1
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.grepformat = "%f:%l:%c:%m"
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        'nvim-lualine/lualine.nvim',
+        depedencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            theme = "nord",
+            icons_enabled = true,
+            components_separators = "|",
+        },
+    }
+}
